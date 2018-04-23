17f7d87d49726546bf74e2c1daeb008ce2137e00
axelKYH <axel.aronsson@student.kyh.se>
2018-04-23 12:23:59 +0200

    added a simple vue instance for testing purpose

--- wp-content/themes/storefront/page.php
+++ wp-content/themes/storefront/page.php
@@ -32,7 +32,23 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+	<div id="app-5">
+		<p>{{ message }}</p>
+		<button v-on:click="reverseMessage">Reverse Message</button>
+	</div>
+<script>
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+</script>
 <?php
 do_action( 'storefront_sidebar' );
 get_footer();
